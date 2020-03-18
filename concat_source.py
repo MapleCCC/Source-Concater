1635db3c2f39b39c3701ba01c3a134da93a2d84c
MapleCCC <littlelittlemaple@gmail.com>
2020-03-18 22:23:33 +0800

    [concated_source] Refacor. Extract graph-related logic code to graph.py

--- concat_source.py
+++ concat_source.py
@@ -3,25 +3,9 @@ import re
 import os
 from typing import *
 from pprint import PrettyPrinter
-from collections import defaultdict
+from graph import *
 
 # print = PrettyPrinter().pprint
-
-Node = str
-Edge = Tuple[Node, Node]
-# Graph is represented internally as data structure adjacency list
-Graph = Dict[Node, Set[Node]]
-
-
-class CircularDependencyError(Exception):
-    pass
-
-
-# TODO: implement Graph data structure as class. More integrated and streamlined.
-# class Graph:
-#     def __init__(self, ):
-#         self._storage =
-
 
 # Naive and simple path join utility.
 # Further revision is required to enhance portability and robustness.
@@ -52,103 +36,19 @@ def extract_dependencies(filepath: str) -> Set[str]:
 # TODO: implement in more sane way. Use queue.
 # TODO: add detection of circular
 def generate_graph(entry: str) -> Graph:
-    graph = dict()
+    graph = Graph()
     waitlist = {entry}
     while waitlist:
         newlist = set()
         for elem in waitlist:
-            if elem not in graph:
+            if not graph.has_node(elem):
                 deps = extract_dependencies(elem)
-                graph[elem] = deps
                 for dep in deps:
-                    if dep not in graph:
+                    graph.add_edge((elem, dep))
+                    if not graph.has_node(dep):
                         newlist.add(dep)
         waitlist = newlist
     return graph
-
-
-def bfs(source: Node, graph: Graph) -> Generator[Node, Any, Any]:
-    assert source in graph
-    q = [source]
-    traversed = set()
-    while q:
-        for node in q:
-            yield node
-            traversed.add(node)
-            for child in graph[node]:
-                if child not in traversed:
-                    q.append(child)
-
-
-def dfs(source: Node, graph: Graph) -> Generator[Node, Any, Any]:
-    assert source in graph
-    stack = [source]
-    traversed = set()
-    while stack:
-        for node in stack[::-1]:
-            yield node
-            traversed.add(node)
-            for child in graph[node]:
-                if child not in traversed:
-                    stack.append(child)
-
-
-def detect_back_edge(source: Node, graph: Graph) -> Optional[Edge]:
-    assert source in graph
-    stack = [source]
-    current_path = [None]
-    while stack:
-        node = stack[-1]
-        if node != current_path[-1]:
-            current_path.append(node)
-            for child in graph[node]:
-                if child in current_path:
-                    return (node, child)
-                stack.append(child)
-        else:
-            stack.pop()
-            current_path.pop()
-    return None
-
-
-def get_invert_graph(graph: Graph) -> Graph:
-    new_graph: Graph = dict()
-    for key in graph.keys():
-        new_graph[key] = set()
-    for node, children in graph.items():
-        for child in children:
-            new_graph[child].add(node)
-    return new_graph
-
-
-def copy_graph(graph: Graph) -> Graph:
-    new_graph = dict()
-    for node, children in graph.items():
-        new_graph[node] = children.copy()
-    return new_graph
-
-
-def topological_sort(graph: Graph) -> Generator[Node, Any, Any]:
-    def find_sources(graph: Graph) -> Set[Node]:
-        sources = set(graph.keys())
-        for children in graph.values():
-            for child in children:
-                if child in sources:
-                    sources.remove(child)
-        return sources
-
-    def remove_sources(graph: Graph) -> Set[Node]:
-        srcs = find_sources(graph)
-        for src in srcs:
-            del graph[src]
-        return srcs
-
-    _graph = copy_graph(graph)
-    srcs = remove_sources(_graph)
-    while srcs:
-        for src in srcs:
-            yield src
-        srcs = remove_sources(_graph)
 
 
 def get_file_content(filepath: str) -> str:
@@ -183,14 +83,14 @@ def main():
     entry = args.entry
 
     graph = generate_graph(entry)
-    back_edge = detect_back_edge(args.entry, graph)
+    back_edge = graph.detect_back_edge(entry)
     if back_edge:
         raise CircularDependencyError(
             f"Circular dependency detected!: '{back_edge[0]}'->'{back_edge[1]}'"
         )
 
-    inv = get_invert_graph(graph)
-    topo_sorted = list(topological_sort(inv))
+    inv = graph.get_invert_graph()
+    topo_sorted = list(inv.topological_sort())
     headers = topo_sorted[:-1]
     implems = list(filter(None, map(get_implem_from_header, headers[::-1])))
     concated = headers + [entry] + implems  # type: ignore
---
+++ graph.py
@@ -0,0 +1,111 @@
+from typing import *
+from collections import defaultdict
+
+
+Node = str
+Edge = Tuple[Node, Node]
+AdjacencyList = Dict[Node, Set[Node]]
+
+
+class CircularDependencyError(Exception):
+    pass
+
+
+# Graph is represented internally as data structure adjacency list
+class Graph:
+    def __init__(self) -> None:
+        self._adjacency_list: AdjacencyList = defaultdict(set)
+
+    __slots__ = "_adjacency_list"
+
+    def add_edge(self, edge: Edge) -> None:
+        v, w = edge
+        self._adjacency_list[v].add(w)
+
+    def has_node(self, node: Node) -> bool:
+        return node in self._adjacency_list
+
+    def bfs(self, source: Node) -> Generator[Node, Any, Any]:
+        assert source in self._adjacency_list
+        q = [source]
+        traversed = set()
+        while q:
+            for node in q:
+                yield node
+                traversed.add(node)
+                for child in self._adjacency_list[node]:
+                    if child not in traversed:
+                        q.append(child)
+
+    def dfs(self, source: Node) -> Generator[Node, Any, Any]:
+        assert source in self._adjacency_list
+        stack = [source]
+        traversed = set()
+        while stack:
+            for node in stack[::-1]:
+                yield node
+                traversed.add(node)
+                for child in self._adjacency_list[node]:
+                    if child not in traversed:
+                        stack.append(child)
+
+    def detect_back_edge(self, source: Node) -> Optional[Edge]:
+        assert source in self._adjacency_list
+        stack = [source]
+        current_path = [None]
+        while stack:
+            node = stack[-1]
+            if node != current_path[-1]:
+                current_path.append(node)
+                for child in self._adjacency_list[node]:
+                    if child in current_path:
+                        return (node, child)
+                    stack.append(child)
+            else:
+                stack.pop()
+                current_path.pop()
+        return None
+
+    def get_invert_graph(self) -> "Graph":
+        new_adjlist = dict()
+        for key in self._adjacency_list.keys():
+            new_adjlist[key] = set()
+        for node, children in self._adjacency_list.items():
+            for child in children:
+                new_adjlist[child].add(node)
+
+        new_graph = Graph()
+        new_graph._adjacency_list = new_adjlist
+        return new_graph
+
+    def copy(self) -> "Graph":
+        # Deep copy
+        new_adjlist = dict()
+        for node, children in self._adjacency_list.items():
+            new_adjlist[node] = children.copy()
+        new_graph = Graph()
+        new_graph._adjacency_list = new_adjlist
+        return new_graph
+
+    def topological_sort(self) -> Generator[Node, Any, Any]:
+        def find_sources(graph: Graph) -> Set[Node]:
+            adjlist = graph._adjacency_list
+            sources = set(adjlist.keys())
+            for children in adjlist.values():
+                for child in children:
+                    if child in sources:
+                        sources.remove(child)
+            return sources
+
+        def remove_sources(graph: Graph) -> Set[Node]:
+            srcs = find_sources(graph)
+            for src in srcs:
+                del graph._adjacency_list[src]
+            return srcs
+
+        _graph = self.copy()
+        srcs = remove_sources(_graph)
+        while srcs:
+            for src in srcs:
+                yield src
+            srcs = remove_sources(_graph)
