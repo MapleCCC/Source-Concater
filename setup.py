806d43cd27b0222c728ff5ef8c9a32c5ebce55f4
MapleCCC <littlelittlemaple@gmail.com>
2020-04-16 16:15:12 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+from concat import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Source Code Concater",
+    author="MapleCCC",
+    author_email="littlelittlemaple@gmail.com",
+    description="...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MapleCCC/Source-Concater",
+    version=__version__,
+    packages=setuptools.find_packages(),
+    license="WTFPL 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=open("requirements.txt", "r").read().splitlines(),
+    entry_points={
+        "console_scripts": [
+            "concat=concat.concat_source:main",
+            # "concat-build=concat.concat_source:",
+        ]
+    },
+)
