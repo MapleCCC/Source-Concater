bd40080c6cd189260a71869e1e1fe0363afdd42d
MapleCCC <littlelittlemaple@gmail.com>
2020-05-09 00:56:57 +0800

    Precompile frequently used regular expressions to accelerate

--- concat/constants.py
+++ concat/constants.py
@@ -1,16 +1,16 @@
+import re
 from pathlib import Path
 
 __all__ = ["INCLUDE_NON_STD_LIB_PATTERN", "INCLUDE_STD_LIB_PATTERN", "IS_HEADER_FILE"]
 
-# TODO: precompile frequently used regular expressions.
 # TODO: relax the regex
 # TODO: Take care of the unlikely case when standard lib is surrounded by "" instead of <> in the #include directive.
 # TODO: take care of any corner/edge cases that we could think of.
 # TODO: it's possible to include .cpp files
 # TODO: it's possible to include .hpp files
 # TODO: it's possible to include .tpp files
-INCLUDE_NON_STD_LIB_PATTERN = r"#include\s+\"(.*\.(?:h|hpp|tpp))\""
-INCLUDE_STD_LIB_PATTERN = r"#include\s+<(.*)>"
+INCLUDE_NON_STD_LIB_PATTERN = re.compile(r"#include\s+\"(.*\.(?:h|hpp|tpp))\"")
+INCLUDE_STD_LIB_PATTERN = re.compile(r"#include\s+<(.*)>")
 
 # TODO: rename "standard lbrary" to "system header"
 
